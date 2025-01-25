31d1cd1b4c88b191a39ac86241d0717f0f2fbf98
harishyadav2706 <harishyadav2706@gmail.com>
2025-01-25 14:46:07 +0530

    first code

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 import gradio as gr
 
-template = """Question: {question}
-
-Answer: Let's think step by step."""
+template = """You act as interactive AI Chatbot, Who help user to provide answer to their question. 
+Question: {question}
+"""
 
 prompt = ChatPromptTemplate.from_template(template)
 
@@ -13,15 +13,35 @@ model = OllamaLLM(model="gemma")
 chain = prompt | model
 
 def generate(question, history):
-    return chain.invoke({"question": {question}})
+    return chain.invoke({"question": question, "history": history})
 
-gr.ChatInterface(
-    generate,
-    type="messages",
-    chatbot=gr.Chatbot(height=300),
-    textbox=gr.Textbox(placeholder="Ask me any question", container=False, scale=7),
-    title="AI Companion",
-    description="Ask AI Companion any question",
-    theme="ocean",
-    cache_examples=True,
-).launch()
+with gr.Blocks(theme="soft", fill_height=True, fill_width=True) as demo:
+    with gr.Row(equal_height=True, variant="panel"):
+        gr.Markdown(
+            "# AI Companion",
+            container=True
+        )
+    with gr.Row():
+        with gr.Column():
+            gr.Image(
+                "https://th.bing.com/th/id/OIP.6WOkEVbGywLjo5gTZXJmgwHaEW?rs=1&pid=ImgDetMain",
+                elem_id="ollama-logo",
+                show_download_button=False,
+                show_label=False,
+                height=180,
+                width=300,
+                show_fullscreen_button=False
+            )
+            gr.Markdown(
+            "### An AI companion chatbot is a virtual assistant designed to provide personalized, human-like interactions. Available 24/7, it adapts to user needs, offering support for tasks, casual conversations, and emotional well-being. From productivity to mental health and learning, AI chatbots enhance daily life with intelligent, empathetic engagement.",
+            container=True
+        )
+
+        with gr.Column(scale=3):
+            gr.ChatInterface(
+                generate,
+                multimodal=True,
+                type="messages",
+            )
+        
+demo.launch()
